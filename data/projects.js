45571dab30f07b25a76c351ba34c33f372820462
techpathi <venkat4coding@gmail.com>
2021-10-11 18:42:34 +0530

    Add Projects & Skills

---
+++ data/projects.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: 'CodeSlash',
+    subtitle: 'ReactJs',
+    description:
+      'A HTML, CSS and JS code editor with web preview feature built using ReactJS and Ace Editor package.',
+    image: './project1.png',
+    link: 'https://codeslash.techpathi.dev'
+  },
+  {
+    title: 'SpaceX Watch',
+    subtitle: 'ReactJs',
+    description:
+      'A realtime SpaceX launch listing directory with live data from SpaceX APIs (docs.spacexdata.com/) ',
+    image: './project4.png',
+    link: 'https://spacexwatch.techpathi.dev'
+  },
+  {
+    title: 'VaccSearch',
+    subtitle: 'ReactJs',
+    description:
+      'A React application using the public COWIN APIs that facilitate users to search for COVID-19 vaccine slots.',
+    image: './project2.png',
+    link: 'https://vaccsearch.techpathi.dev'
+  },
+  {
+    title: 'Voice of Vignan',
+    subtitle: 'Android and Firebase',
+    description:
+      'Voice of Vignan is an Android application to deliver instant updates for my undergrad college.',
+    image: './project3.png',
+    link: 'https://play.google.com/store/apps/details?id=com.dscviit.vov'
+  }
+];
+
+export const skills = [
+  'JavaScript',
+  'ReactJs',
+  'Node/Express.js',
+  'MongoDB',
+  'HTML',
+  'CSS',
+  'Bootstrap',
+  'Ant Design for React',
+  'Android',
+  'Git'
+];
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,94 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from 'next/image';
+import HeroImage from '../public/hero.svg';
+import SkillLegend from '../public/skilllegend.svg';
+import utilStyles from '../styles/utils.module.css';
+import { projects, skills } from '../data/projects';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={utilStyles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Venkatapathi Raju Gangiri</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        <section className={utilStyles.heroContainer}>
+          <div className={utilStyles.heroTextBlock}>
+            <p className={`${utilStyles.headingXl} ${utilStyles.headerFont}`}>
+              Hi, I'm Venkatapathi Raju. I love to build amazing web apps.
             </p>
-          </a>
-        </div>
+            <p
+              className={`${utilStyles.headingLg} ${utilStyles.subHeaderFont}`}
+            >
+              I speak JavaScript. I would buy you a ☕️ if you can explain me
+              this. I am interested in Space tech. If you often watch the SpaceX
+              launches, you got me! Sometimes I scribble my thoughts on my
+              Medium blog.
+            </p>
+          </div>
+          <div className={utilStyles.heroImageBlock}>
+            <Image priority src={HeroImage} layout='responsive' />
+          </div>
+        </section>
+        <section id='projects' className={utilStyles.projectContainer}>
+          <p
+            className={`${utilStyles.headingXl} ${utilStyles.headerFont} ${utilStyles.centerText}`}
+          >
+            Web apps I built for fun
+          </p>
+          <div className={utilStyles.projectGrid}>
+            {projects.map(project => {
+              return (
+                <div
+                  className={utilStyles.projectCard}
+                  href={project.link}
+                  target='_blank'
+                >
+                  <p
+                    className={`${utilStyles.headingLg} ${utilStyles.headerFont} ${utilStyles.centerText}`}
+                  >
+                    {project.title}
+                  </p>
+                  <p>{project.subtitle}</p>
+                  <p
+                    className={`${utilStyles.headingMd} ${utilStyles.subHeaderFont}`}
+                  >
+                    {project.description}
+                  </p>
+
+                  <a
+                    className={utilStyles.appLink}
+                    href={project.link}
+                    target='_blank'
+                  >
+                    View the app
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        <section id='skills' className={utilStyles.projectContainer}>
+          <p
+            className={`${utilStyles.headingXl} ${utilStyles.headerFont} ${utilStyles.centerText}`}
+          >
+            Skills in my arsenal
+          </p>
+          <div className={utilStyles.skillGrid}>
+            {skills.map(skill => {
+              return (
+                <div
+                  className={`${utilStyles.skillRow} ${utilStyles.centerText}`}
+                >
+                  <Image priority src={SkillLegend} width={60} height={60} />
+                  <span>{skill}</span>
+                </div>
+              );
+            })}
+          </div>
+        </section>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
-  )
+  );
 }

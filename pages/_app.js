157d7e5ffd30decc4118ab576b51fbb343234571
techpathi <venkat4coding@gmail.com>
2021-10-12 20:28:24 +0530

    Add Social links

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import '../styles/global.css';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head';
 import Image from 'next/image';
 import HeroImage from '../public/hero.svg';
 import SkillLegend from '../public/skilllegend.svg';
+import SkillTag from '../public/skilltag.svg';
+import InstaIcon from '../public/instagram.svg';
+import TwitterIcon from '../public/twitter.svg';
+import LinkedinIcon from '../public/linkedin.svg';
+import MediumIcon from '../public/medium.svg';
+import MailIcon from '../public/mail.svg';
 import utilStyles from '../styles/utils.module.css';
 import { projects, skills } from '../data/projects';
 
@@ -24,7 +30,13 @@ export default function Home() {
               I speak JavaScript. I would buy you a ☕️ if you can explain me
               this. I am interested in Space tech. If you often watch the SpaceX
               launches, you got me! Sometimes I scribble my thoughts on my
-              Medium blog.
+              Medium{' '}
+              <span>
+                <a href='https://techpathi.medium.com' target='_blank'>
+                  {' '}
+                  blog.
+                </a>
+              </span>
             </p>
           </div>
           <div className={utilStyles.heroImageBlock}>
@@ -35,28 +47,28 @@ export default function Home() {
           <p
             className={`${utilStyles.headingXl} ${utilStyles.headerFont} ${utilStyles.centerText}`}
           >
-            Web apps I built for fun
+            Apps I Built For Fun
           </p>
           <div className={utilStyles.projectGrid}>
             {projects.map(project => {
               return (
-                <div
-                  className={utilStyles.projectCard}
-                  href={project.link}
-                  target='_blank'
-                >
+                <div className={utilStyles.projectCard}>
                   <p
                     className={`${utilStyles.headingLg} ${utilStyles.headerFont} ${utilStyles.centerText}`}
                   >
                     {project.title}
                   </p>
-                  <p>{project.subtitle}</p>
+                  <p>
+                    {project.subtitle}{' '}
+                    <span>
+                      <Image priority src={SkillTag} width={15} height={15} />
+                    </span>
+                  </p>
                   <p
                     className={`${utilStyles.headingMd} ${utilStyles.subHeaderFont}`}
                   >
                     {project.description}
                   </p>
-
                   <a
                     className={utilStyles.appLink}
                     href={project.link}
@@ -73,7 +85,7 @@ export default function Home() {
           <p
             className={`${utilStyles.headingXl} ${utilStyles.headerFont} ${utilStyles.centerText}`}
           >
-            Skills in my arsenal
+            Skills In My Arsenal
           </p>
           <div className={utilStyles.skillGrid}>
             {skills.map(skill => {
@@ -86,6 +98,31 @@ export default function Home() {
                 </div>
               );
             })}
+          </div>
+        </section>
+
+        <section id='skills' className={utilStyles.projectContainer}>
+          <p
+            className={`${utilStyles.headingXl} ${utilStyles.headerFont} ${utilStyles.centerText}`}
+          >
+            I am social!!! let's connect
+          </p>
+          <div className={utilStyles.socialGrid}>
+            <a href='https://www.instagram.com/iamtechpathi/' target='_blank'>
+              <Image priority src={InstaIcon} width={60} height={60} />{' '}
+            </a>
+            <a href='https://twitter.com/iamtechpathi' target='_blank'>
+              <Image priority src={TwitterIcon} width={60} height={60} />
+            </a>
+            <a href='https://www.linkedin.com/in/gvpraju/' target='_blank'>
+              <Image priority src={LinkedinIcon} width={60} height={60} />
+            </a>
+            <a href='https://www.techpathi.medium.com/' target='_blank'>
+              <Image priority src={MediumIcon} width={60} height={60} />
+            </a>
+            <a href='mailto:venkat4coding@gmail.com' target='_blank'>
+              <Image priority src={MailIcon} width={60} height={60} />
+            </a>
           </div>
         </section>
       </main>
